fd2bc031a98983fea3c535582dd98aa7057f8e0a
Kai <kai@ThinkpadX1Yoga.fritz.box>
2018-01-13 01:46:36 +0100

    Erster Commit

---
+++ Debug/HAL_Driver/Src/stm32f1xx_hal_crc.d
@@ -0,0 +1,154 @@
+HAL_Driver/Src/stm32f1xx_hal_crc.o: ../HAL_Driver/Src/stm32f1xx_hal_crc.c \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_conf.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ /home/kai/ARM/Temp_Sensor/CMSIS/device/stm32f1xx.h \
+ /home/kai/ARM/Temp_Sensor/CMSIS/device/stm32f103xb.h \
+ /home/kai/ARM/Temp_Sensor/CMSIS/core/core_cm3.h \
+ /home/kai/ARM/Temp_Sensor/CMSIS/core/core_cmInstr.h \
+ /home/kai/ARM/Temp_Sensor/CMSIS/core/cmsis_gcc.h \
+ /home/kai/ARM/Temp_Sensor/CMSIS/core/core_cmFunc.h \
+ /home/kai/ARM/Temp_Sensor/CMSIS/device/system_stm32f1xx.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_eth.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_can.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_can_ex.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_cec.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_adc.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_crc.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_dac.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_sram.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_ll_fsmc.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_nor.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_i2c.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_i2s.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_iwdg.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_rtc.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_rtc_ex.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_pccard.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_sd.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_nand.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_spi.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_uart.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_usart.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_irda.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_smartcard.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_wwdg.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_pcd.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_ll_usb.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h \
+ /home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_hcd.h
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_conf.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+/home/kai/ARM/Temp_Sensor/CMSIS/device/stm32f1xx.h:
+
+/home/kai/ARM/Temp_Sensor/CMSIS/device/stm32f103xb.h:
+
+/home/kai/ARM/Temp_Sensor/CMSIS/core/core_cm3.h:
+
+/home/kai/ARM/Temp_Sensor/CMSIS/core/core_cmInstr.h:
+
+/home/kai/ARM/Temp_Sensor/CMSIS/core/cmsis_gcc.h:
+
+/home/kai/ARM/Temp_Sensor/CMSIS/core/core_cmFunc.h:
+
+/home/kai/ARM/Temp_Sensor/CMSIS/device/system_stm32f1xx.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_eth.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_can.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_can_ex.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_cec.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_adc.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_crc.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_dac.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_sram.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_ll_fsmc.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_nor.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_i2c.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_i2s.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_iwdg.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_rtc.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_rtc_ex.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_pccard.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_sd.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_nand.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_spi.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_uart.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_usart.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_irda.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_smartcard.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_wwdg.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_pcd.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_ll_usb.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h:
+
+/home/kai/ARM/Temp_Sensor/HAL_Driver/Inc/stm32f1xx_hal_hcd.h:
